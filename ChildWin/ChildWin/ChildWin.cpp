d9e59c867723bc6ef066f766eca7fb1ff47fcaf8
熊 九龙 <410946628@qq.com>
2018-11-19 16:30:04 +0800

    2018年11月19日16:30:01

--- ChildWin/ChildWin/ChildWin.cpp
+++ ChildWin/ChildWin/ChildWin.cpp
@@ -3,12 +3,9 @@
 文件：ChildWin.c
 功能：子窗口演示程序——命中测试
 ********************************************************************/
+
 #include "stdafx.h"
-#include "resource.h"
-#include <windows.h>
 #include <windowsx.h>
-
-
 
 #define ROW_NUM 4     //行数
 #define COL_NUM 8     //列数
